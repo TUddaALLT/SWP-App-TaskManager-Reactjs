322e17c6dbbea2396e62289f9f92f5f80dc6363b
tudda <tentufancr7@gmail.com>
2023-03-02 11:55:02 +0700

    not found page

--- fe_swp/src/components/NotFoundPage.js
+++ fe_swp/src/components/NotFoundPage.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div>
+      <div>
+        <img src='https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=630' />
+        <span style={{ marginRight: "20px", fontSize: "30px" }}>
+          You should login to continue
+        </span>
+
+        <Button size='medium'>
+          {" "}
+          <a href='/Login'>Login</a>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
