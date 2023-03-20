a7d2d9e92b2267193bf62005d4f129c2063c1ea0
tudda <tentufancr7@gmail.com>
2023-03-21 00:33:27 +0700

    :))

--- fe_swp/src/components/TaskDetail.js
+++ fe_swp/src/components/TaskDetail.js
@@ -1,45 +1,40 @@
 import React, { useState } from "react";
-import '../styles/TaskDetail.css';
-import PushPinIcon from '@mui/icons-material/PushPin';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import ReviewsIcon from '@mui/icons-material/Reviews';
+import "../styles/TaskDetail.css";
+import PushPinIcon from "@mui/icons-material/PushPin";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import ReviewsIcon from "@mui/icons-material/Reviews";
 
 function TaskDetailComponent({ task }) {
-
-    return (
-        <div className="Task-Detail">
-            <div className="Task-Detail-Title">
-                <h1>{task.title}</h1>
-                <PushPinIcon className={`pin-task-icon ${task.pinTask == true ? " pin-task-true" : " pin-task-false"}`} />
-            </div>
-            <div className="Task-description">
-                <p>{task.describe}</p>
-            </div>
-            <div className="Task-Line-3">
-                <div className="Task-detail-status">
-
-                    <div className="AttackIcon">
-                        <AttachFileIcon />
-                    </div>
-                    <div className="CommendIcon">
-                        <div className="number-cmt">1</div>
-                        <ReviewsIcon />
-                    </div>
-
-                </div>
-                <div className="PeopleJoin">
-                    <div className="PeopleJoin-img">
-
-                    </div>
-                    <div className="PeopleJoin-img">
-
-                    </div>
-                    <div className="PeopleJoin-img">
-
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='Task-Detail'>
+      <div className='Task-Detail-Title'>
+        <h1>{task.title}</h1>
+        <PushPinIcon
+          className={`pin-task-icon ${
+            task.pinTask == true ? " pin-task-true" : " pin-task-false"
+          }`}
+        />
+      </div>
+      <div className='Task-description'>
+        <p>{task.describe}</p>
+      </div>
+      <div className='Task-Line-3'>
+        <div className='Task-detail-status'>
+          <div className='AttackIcon'>
+            <AttachFileIcon />
+          </div>
+          <div className='CommendIcon'>
+            <div className='number-cmt'>1</div>
+            <ReviewsIcon />
+          </div>
         </div>
-    )
+        <div className='PeopleJoin'>
+          <div className='PeopleJoin-img'></div>
+          <div className='PeopleJoin-img'></div>
+          <div className='PeopleJoin-img'></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default TaskDetailComponent;
--- fe_swp/src/components/TaskListPickDay.js
+++ fe_swp/src/components/TaskListPickDay.js
@@ -1,34 +1,41 @@
-import React, { useState , useEffect} from "react";
-import Grid from '@mui/material/Grid';
+import React, { useState, useEffect } from "react";
 import TaskDetailComponent from "./TaskDetail";
 
 const Calendar = (props) => {
-    const [ListTasks, setListTasks] = useState([]);
-    useEffect(() => {
-        setListTasks([]);
-        fetch('https://localhost:7239/api/Task/GetTasksRangeByTime?userID=1&timeFrom=' + props.daystring +'&timeTo=' + props.daystring)
-           .then((response) => response.json())
-           .then((data) => {
-              setListTasks(data.data);
-              console.log('https://localhost:7239/api/Task/GetTasksRangeByTime?userID=1&timeFrom=' + props.daystring +'&timeTo=' + props.daystring)
-            
-            })
-           .catch((err) => {
-             
-            console.log(err.message);
-           });
-     }, [props.daystring]);
+  const [ListTasks, setListTasks] = useState([]);
+  useEffect(() => {
+    setListTasks([]);
+    fetch(
+      `https://localhost:7239/api/Task/GetTasksRangeByTime?userID=${localStorage.getItem(
+        "id",
+      )}&timeFrom=` +
+        props.daystring +
+        "&timeTo=" +
+        props.daystring,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setListTasks(data.data);
+        console.log(
+          `https://localhost:7239/api/Task/GetTasksRangeByTime?userID=${localStorage.getItem(
+            "id",
+          )}&timeFrom=` +
+            props.daystring +
+            "&timeTo=" +
+            props.daystring,
+        );
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [props.daystring]);
 
-    return (
-        <div className="Task-list">
-            
-
-            {ListTasks.map((Task) => (
-                <TaskDetailComponent task={Task}/>
-                
-            ))}
-          
-        </div>
-    );
+  return (
+    <div className='Task-list' style={{ height: "65vh", overflowY: "auto" }}>
+      {ListTasks.map((Task) => (
+        <TaskDetailComponent key={Task.id} task={Task} />
+      ))}
+    </div>
+  );
 };
 export default Calendar;
